00d4e7ff35715195d754b268065633c277d85af6
HenryKasai <henrykasai1775@gmail.com>
2020-04-30 20:42:32 -0300

    Exercício resolvido.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Insira seus dados e veja seu índice de massa corporal (IMC).");
+
+            Console.Write("Altura em metros: ");
+            string h = Console.ReadLine();
+            double altura = Convert.ToDouble(h);
+
+            Console.Write("Massa em quilogramas (peso): ");
+            string m = Console.ReadLine();
+            double massa = Convert.ToDouble(m);
+
+            double result = Math.Pow(altura, 2);
+            double resultfinal = massa / result;
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("O índice de massa corporal em questão é " + resultfinal + " kg/m².");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+
+
+        }
+    }
+}
